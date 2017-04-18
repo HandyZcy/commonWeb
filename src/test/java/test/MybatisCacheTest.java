c004ea95e36a7338290af4f75f75f934c58883d3
zhangcy <1426412031@qq.com>
2017-04-18 16:48:36 +0800

    调整JunitTest测试类

--- src/test/java/test/MybatisCacheTest.java
+++ src/test/java/test/MybatisCacheTest.java
@@ -1,16 +1,11 @@
 package test;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.system.service.RightService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath*:/config/spring/spring-config.xml"})
-public class MybatisCacheTest {
+public class MybatisCacheTest extends BaseJunitTest{
 
 	@Autowired
 	private RightService rightService;
--- src/test/java/test/RedisTest.java
+++ src/test/java/test/RedisTest.java
@@ -1,14 +1,39 @@
 package test;
 
-import static org.junit.Assert.*;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 
-public class RedisTest {
+public class RedisTest extends BaseJunitTest{
 
+	@Autowired
+	private RedisTemplate<Serializable, Serializable> redisTemplate;
+	
+	JdkSerializationRedisSerializer jdkSerialization = new JdkSerializationRedisSerializer();
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Long dbSize = redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) throws DataAccessException {
+				return connection.dbSize();
+			}
+		});
+		System.out.println(dbSize);
+		Set<Serializable> keySet = redisTemplate.keys("*");
+		Iterator<Serializable> iterator = keySet.iterator();
+		while (iterator.hasNext()) {
+			String key = (String) iterator.next();
+			System.out.println(key);
+		}
 	}
 
 }
